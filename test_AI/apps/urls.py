520e222dc8db64c7e225eda5423e7fa38bedb899
vaibhav-exe <vaibhav0000.vg@gmail.com>
2026-01-18 20:22:29 +0530

    made a to-do web app

--- test_AI/apps/urls.py
+++ test_AI/apps/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("",index,name="index")
+    #path("route_naming", function_name, name="any_prefered_name") 
+    path("",index,name="index"),
+    path("create/", to_do_create, name="to_do_create"),
+    path("list/", to_do_list, name="to_do_list"),
+    path("update/", to_do_update, name="to_do_update"),
+    path("view/", to_do_view, name="to_do_view"),
+    
 ]
--- test_AI/apps/views.py
+++ test_AI/apps/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(requests):
-    return render(requests,"index.html")
+def index(request):
+    return render(request,"index.html")
+
+def to_do_create(request):
+    return render(request,"to_do_create.html")
+
+def to_do_list(request):
+    return render(request,"to_do_list.html")
+
+def to_do_update(request):
+    return render(request,"to_do_update.html")
+
+def to_do_view(request):
+    return render(request,"to_do_view.html")
